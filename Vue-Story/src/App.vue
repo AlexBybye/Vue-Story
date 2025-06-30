e9ad4c0ea8b1c05caae95e8d36d6c08ffa3634cd
Alexbybye <244417287@qq.com>
2025-07-01 01:21:34 +0800

    Warning Backup

--- Vue-Story/src/App.vue
+++ Vue-Story/src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <h1>根组件</h1>
+    <h1>
+      根组件
+    </h1>
+    <el-button type="primary" size="default" :icon="Plus">
+      Main Button
+    </el-button>
   </div>
 </template>
 
 <script setup>
+import {Plus} from '@element-plus/icons-vue'
 const str ="This is a start"
 </script>
 
--- Vue-Story/vite.config.ts
+++ Vue-Story/vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // ...
+  plugins: [
+    // ...
+    [vue()],
+
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      symbolId: 'icon-[dir]-[name]',
+    }),
+
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+    },
+  },
 })
+
